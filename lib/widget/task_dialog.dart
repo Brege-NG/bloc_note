4b277a883471594748d7536a96ed7d3aa1fcc3b5
rv-gates <ngoulouherve@gmail.com>
2024-07-16 16:25:32 +0200

    ajout d'un text dans la page principale

--- lib/widget/task_dialog.dart
+++ lib/widget/task_dialog.dart
@@ -30,6 +30,9 @@ class TaskDialogState extends ConsumerState<TaskDialog> {
       title: Column(
         children: [
           const Text("ajoutez une tache ici svp"),
+          const Text("pas trop long svp", style: TextStyle(
+            fontSize: 10.0, color: Colors.red
+          ),),
           const SizedBox(
             height: 5.0,
           ),
